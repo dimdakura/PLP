43756b5342d25a2bb3529704af039119551a5ff7
dimdakura <54310982+dimdakura@users.noreply.github.com>
2024-02-26 12:20:11 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = input("what is your age:")
+location = input("where is your location:")
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+print(message)
